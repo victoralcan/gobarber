708f3fadb89b83a2c9998cf0d13cbea0df647bdc
victoralcan <victoralcan@gmail.com>
2020-02-19 18:01:06 -0300

    Configuraçao de template de email usando hbs

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -131,6 +131,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -155,7 +160,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento Cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: ptBR,
+        }),
+      },
     });
 
     return res.json(appointment);
